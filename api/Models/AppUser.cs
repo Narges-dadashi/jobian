a3281796cd7ac9405fde19f23d7dea7e02a030c4
Narges-dadashi <nargesdadashi7@gmail.com>
2025-04-30 19:32:30 +0330

    create AppUser Model

--- api/Models/AppUser.cs
+++ api/Models/AppUser.cs
@@ -1,5 +1,11 @@
 namespace api.Models;
 
 public record AppUser(
-
+[EmailAddress] string Email,
+[Length(3, 30)] string Name,
+[Length(8, 16)] string Password,
+string ConfirmPassword,
+[Range(18, 50)] int Age,
+[Length(3, 30)] string City,
+[Length(3, 30)] string Country
 );
