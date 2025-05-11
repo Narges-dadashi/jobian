6f2586e3b6fd6ae35db3c4a456d4f9ad18dc1292
Narges-dadashi <nargesdadashi7@gmail.com>
2025-05-11 11:37:31 +0330

    Account Service & LocalStorage

---
+++ client/src/app/services/account.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { LoggedIn } from '../../../models/logged-in.model';
+import { Login } from '../../../models/login.model';
+import { AppUser } from '../../../models/app-user.model';
+import { Member } from '../../../models/member.model';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  http = inject(HttpClient);
+  private readonly _baseApiUrl: string = 'http://localhost:5000/api/';
+  platformId = inject(PLATFORM_ID);
+
+  register(user: AppUser): Observable<LoggedIn> {
+    return this.http.post<LoggedIn>(this._baseApiUrl + 'account/register', user);
+  }
+
+  login(userInput: Login): Observable<LoggedIn> {
+    return this.http.post<LoggedIn>(
+      this._baseApiUrl + 'account/login', userInput).pipe(
+        map(userResponse => {
+          this.setCurrentUser(userResponse);
+
+          return userResponse;
+        })
+      )
+  }
+
+  getAllMember(loggedInUser: LoggedIn): Observable<Member[]> {
+    return this.http.get<Member[]>(this._baseApiUrl + 'member');
+  }
+
+  setCurrentUser(loggedInUser: LoggedIn): void {
+    if (isPlatformBrowser(this.platformId))
+      localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser));
+  }
+}
