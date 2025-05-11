70153457feda432b3b57604f4c6e2b0d554875cd
Narges-dadashi <nargesdadashi7@gmail.com>
2025-05-11 12:04:20 +0330

    Login Component

--- client/src/app/components/account/login/login.component.ts
+++ client/src/app/components/account/login/login.component.ts
@@ -1,14 +1,48 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AccountService } from '../../../services/account.service';
+import { Login } from '../../../../../models/login.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule, ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule, MatFormFieldModule, MatInputModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  accountService = inject(AccountService);
   fB = inject(FormBuilder);
 
+  loginFg = this.fB.group({
+    emailCtrl: ['', [Validators.required, Validators.email]],
+    passwordCtrl: ['']
+  })
+
+  get EmailCtrl(): FormControl {
+    return this.loginFg.get('emailCtrl') as FormControl;
+  }
+
+  get PasswordCtrl(): FormControl {
+    return this.loginFg.get('passwordCtrl') as FormControl;
+  }
+
+  login(): void {
+    let userInput: Login = {
+      email: this.EmailCtrl.value,
+      password: this.PasswordCtrl.value
+    }
+
+    this.accountService.login(userInput).subscribe();
+  }
 }
