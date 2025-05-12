a8afdd829de989c6588c412f9c57210a345a96fe
Narges-dadashi <nargesdadashi7@gmail.com>
2025-05-12 10:57:15 +0330

    RegisterComponent

--- client/src/app/components/account/register/register.component.ts
+++ client/src/app/components/account/register/register.component.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AccountService } from '../../../services/account.service';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppUser } from '../../../../../models/app-user.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule, ReactiveFormsModule,
+    MatButtonModule, MatInputModule, MatFormFieldModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  accountService = inject(AccountService);
+  fB = inject(FormBuilder);
 
+  registerFg = this.fB.group({
+    emailCtrl: ['', [Validators.required, Validators.email]],
+    nameCtrl: '',
+    passwordCtrl: '',
+    confirmPasswordCtrl: '',
+    ageCtrl: 0,
+    cityCtrl: '',
+    countryCtrl: ''
+  });
+
+  get EmailCtrl(): FormControl {
+    return this.registerFg.get('emailCtrl') as FormControl;
+  }
+
+  get NameCtrl(): FormControl {
+    return this.registerFg.get('nameCtrl') as FormControl;
+  }
+
+  get PasswordCtrl(): FormControl {
+    return this.registerFg.get('passwordCtrl') as FormControl;
+  }
+
+  get ConfirmPasswordCtrl(): FormControl {
+    return this.registerFg.get('confirmPasswordCtrl') as FormControl;
+  }
+
+  get AgeCtrl(): FormControl {
+    return this.registerFg.get('ageCtrl') as FormControl;
+  }
+
+  get CityCtrl(): FormControl {
+    return this.registerFg.get('cityCtrl') as FormControl;
+  }
+
+  get CountryCtrl(): FormControl {
+    return this.registerFg.get('countryCtrl') as FormControl;
+  }
+
+  register(): void {
+    let user: AppUser = {
+      email: this.EmailCtrl.value,
+      name: this.NameCtrl.value,
+      password: this.PasswordCtrl.value,
+      confirmPassword: this.ConfirmPasswordCtrl.value,
+      age: this.AgeCtrl.value,
+      city: this.CityCtrl.value,
+      country: this.CountryCtrl.value
+    }
+
+    this.accountService.register(user).subscribe({
+      next: (res) => console.log(res),
+      error: (err) => console.log(err.error)
+    });
+  }
 }
