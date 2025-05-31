f3c30cbad0428d94247e73535524afaa9be5f424
Narges-dadashi <nargesdadashi7@gmail.com>
2025-05-31 11:04:12 +0330

    Keep user login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  accountService = inject(AccountService);
+
+  ngOnInit(): void {
+    let loggedInUser: string | null = localStorage.getItem('loggedInUser');
+    console.log(loggedInUser);
+
+    if (loggedInUser != null)
+      this.accountService.setCurrentUser(JSON.parse(loggedInUser));
+  }
 }
