eba48f4cee40c63a1276ac4705879b4d0f4583c6
Narges-dadashi <nargesdadashi7@gmail.com>
2025-06-07 10:08:02 +0330

    Menu in navbar

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,22 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { AccountService } from '../../services/account.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink,
-    MatButtonModule, MatIconModule, MatToolbarModule
+    RouterLink, CommonModule,
+    MatButtonModule, MatIconModule,
+    MatToolbarModule, MatMenuModule,
+    MatDividerModule, MatListModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
