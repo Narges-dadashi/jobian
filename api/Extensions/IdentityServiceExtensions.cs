a31e9d9adf9ed5a812bf5883d42a17ebf1305872
Narges-dadashi <nargesdadashi7@gmail.com>
2025-07-13 17:28:55 +0330

    Add Token Key

--- api/Extensions/IdentityServiceExtensions.cs
+++ api/Extensions/IdentityServiceExtensions.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace api.Extensions;
 
 public static class IdentityServiceExtensions
