927eb3466430d98d2bbd112e40dab5d591354dbf
Narges-dadashi <nargesdadashi7@gmail.com>
2025-10-18 20:12:14 +0330

    Create error components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,20 +7,35 @@ import { FooterComponent } from './components/footer/footer.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { JobComponent } from './components/job/job.component';
 import { authLoggedInGuard } from './guards/auth-logged-in.guard';
+import { authGuard } from './guards/auth.guard';
+import { UserEditComponent } from './components/user/user-edit/user-edit.component';
+import { NoAccessComponent } from './components/errors/no-access/no-access.component';
+import { ServerErrorComponent } from './components/errors/server-error/server-error.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     {
         path: '',
         runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'user/user-edit', component: UserEditComponent },
+            { path: 'no-access', component: NoAccessComponent }
+        ]
+    },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
         canActivate: [authLoggedInGuard],
         children: [
             { path: 'account/login', component: LoginComponent },
-            { path: 'account/register', component: RegisterComponent },
+            { path: 'account/register', component: RegisterComponent }
         ]
     },
     { path: 'navbar', component: NavbarComponent },
     { path: 'footer', component: FooterComponent },
+    { path: 'server-error', component: ServerErrorComponent },
+    { path: '**', component: NotFoundComponent, pathMatch: 'full' },
     { path: '**', component: NotFoundComponent },
     { path: 'job', component: JobComponent }
 ];
