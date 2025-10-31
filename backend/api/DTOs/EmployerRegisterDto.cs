0d8f2be3d420dec1157d10e38a48a6427b076f1a
Narges-dadashi <nargesdadashi7@gmail.com>
2025-10-31 18:07:33 +0330

    Create EmployerRegisterDto

---
+++ backend/api/DTOs/EmployerRegisterDto.cs
@@ -0,0 +1,25 @@
+namespace api.DTOs;
+
+public record EmployerRegisterDto(
+    string Email,
+    string CompanyName,
+    string Password
+);
+
+
+// string CompanyName,
+// string Industry,
+// string CompanyEmail,
+// string Password,
+// string CompanyPhoneNumber,
+// string ContactPhoneNumber,
+// string City,
+// string Province,
+
+// string? About,
+// string? LogoUrl,
+
+// string? ContactPersonName,
+// string? ContactPersonPosition,
+
+// DateTime RegisterDate
