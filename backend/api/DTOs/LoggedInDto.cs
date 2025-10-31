37d1e34fb7e45d7b42099b1085d7a16d18942bf5
Narges-dadashi <nargesdadashi7@gmail.com>
2025-10-31 18:18:01 +0330

    Add RegisterEmployer to IAccountRepository

--- backend/api/DTOs/LoggedInDto.cs
+++ backend/api/DTOs/LoggedInDto.cs
@@ -5,7 +5,6 @@ namespace api.DTOs;
 public record LoggedInDto(
     string Email,
     string UserName,
-    int Age,
     string Token,
     [Optional] string? ProfilePhotoUrl
 );
--- backend/api/Interfaces/IAccountRepository.cs
+++ backend/api/Interfaces/IAccountRepository.cs
@@ -2,7 +2,8 @@ namespace api.Interfaces;
 
 public interface IAccountRepository
 {
-    public Task<LoggedInDto?> RegisterAsync(RegisterDto userInput, CancellationToken cancellationToken);
+    public Task<LoggedInDto?> RegisterJobSeekerAsync(JobSeekerRegisterDto userInput, CancellationToken cancellationToken);
+    public Task<LoggedInDto?> RegisterEmployerAsync(EmployerRegisterDto userInput, CancellationToken cancellationToken);
     public Task<LoggedInDto?> LoginAsync(LoginDto userInput, CancellationToken cancellationToken);
     public Task<DeleteResult?> DeleteByIdAsync(string userId, CancellationToken cancellationToken);
     public Task<LoggedInDto?> ReloadLoggedInUserAsync(string userId, string token, CancellationToken cancellationToken);
