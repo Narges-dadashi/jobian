fb4bb36f87d6d5f5d648b0a2c94a00824ed4c23b
Narges-dadashi <nargesdadashi7@gmail.com>
2025-11-09 19:21:46 +0330

    Create EmployerUpdateDto

---
+++ backend/api/DTOs/EmployerUpdateDto.cs
@@ -0,0 +1,14 @@
+namespace api.DTOs;
+
+public record EmployerUpdateDto(
+    string Industry,
+    string CompanyPhoneNumber,
+    string ContactPhoneNumber,
+    string About,
+    string LogoUrl,
+    string ContactPersonName,
+    string ContactPersonPosition,
+    string City,
+    string Province,
+    DateTime RegisterDate
+);
