ac7323971057c2e75c3a28e04ef0c1732258e574
Narges-dadashi <nargesdadashi7@gmail.com>
2025-11-25 13:37:02 +0330

    Create job-seeker-update

---
+++ client/src/app/models/job-seeker-update.model.ts
@@ -0,0 +1,12 @@
+export interface JobSeekerUpdate {
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    resumeFileUrl: string;
+    bio: string;
+    skills: string[]; 
+    educationLevel: string;
+    experienceYears: number;
+    city: string;
+    province: string;  
+}
