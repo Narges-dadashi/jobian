8323bc04f6adb843e4cf9f64fd3431b638637298
Narges-dadashi <nargesdadashi7@gmail.com>
2025-12-25 21:06:42 +0330

    Add Pagination (client)

---
+++ client/src/app/extensions/paginationHandler.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { map, Observable } from "rxjs";
+import { PaginatedResult } from "../models/helpers/paginatedResult";
+import { inject } from "@angular/core";
+
+export class PaginationHandler {
+    private http = inject(HttpClient);
+
+    /**
+     * A reusable pagination method with generic type
+     * @param url
+     * @param params
+     * @returns Observable<PaginatedResult<T>>
+     */
+    getPaginatedResult<T>(url: string, params: HttpParams): Observable<PaginatedResult<T>> {
+        const paginatedResult = new PaginatedResult<T>();
+
+        return this.http.get<T>(url, { observe: 'response', params })
+            .pipe(
+                map(response => {
+                    const pagination: string | null = response.headers.get('Pagination');
+
+                    if (pagination)
+                        paginatedResult.pagination = JSON.parse(pagination);
+
+                    if (response.body)
+                        paginatedResult.body = response.body
+
+                    return paginatedResult;
+                })
+            );
+    }
+}
