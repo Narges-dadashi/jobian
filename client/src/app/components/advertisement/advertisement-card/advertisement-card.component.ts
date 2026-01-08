26bb60efddc532cf9f960b52ef05c324972b9cad
Narges-dadashi <nargesdadashi7@gmail.com>
2026-01-08 18:49:01 +0330

    Add advertisement card

--- client/src/app/components/advertisement/advertisement-card/advertisement-card.component.ts
+++ client/src/app/components/advertisement/advertisement-card/advertisement-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AdvertisementResponse } from '../../../models/advertisement-response';
+import { environment } from '../../../../environments/environment.development';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-advertisement-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, MatCardModule, MatIconModule, MatButtonModule, 
+    MatTooltipModule
+  ],
   templateUrl: './advertisement-card.component.html',
   styleUrl: './advertisement-card.component.scss'
 })
 export class AdvertisementCardComponent {
-
+  @Input('advertisementInput') advertisementIn: AdvertisementResponse | undefined;
+  apiUrl = environment.apiUrl;
 }
