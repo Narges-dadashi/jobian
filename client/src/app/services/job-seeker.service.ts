c97ce621d5f90dcc961eff4848bcef5b60724b79
Narges-dadashi <nargesdadashi7@gmail.com>
2026-02-04 21:35:37 +0330

    Add job-seeker service

---
+++ client/src/app/services/job-seeker.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { JobSeeker } from '../models/job-seeker.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobSeekerService {
+  http = inject(HttpClient);
+
+  private readonly _baseApiUrl: string = environment.apiUrl + 'api/member/';
+
+  getByUserName(userNameInput: string): Observable<JobSeeker | undefined> {
+    return this.http.get<JobSeeker>(this._baseApiUrl + 'get-by-username/' + userNameInput);
+  }
+}
