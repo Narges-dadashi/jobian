01f45f6f9204317eefdac6fb24799b106123bfc4
Narges-dadashi <nargesdadashi7@gmail.com>
2026-02-26 17:06:33 +0330

    Use advertisement params

--- backend/api/Interfaces/IAdvertisementRepository.cs
+++ backend/api/Interfaces/IAdvertisementRepository.cs
@@ -3,5 +3,5 @@ namespace api.Interfaces;
 public interface IAdvertisementRepository
 {
     public Task<AdvertisementResponseDto?> CreateAdvertisementAsync(Advertisement advertisement, string userId, CancellationToken cancellationToken);
-    public Task<PagedList<Advertisement>> GetAllAdvertisementsAsync(PaginationParams paginationParams, CancellationToken cancellationToken);
+    public Task<PagedList<Advertisement>> GetAllAdvertisementsAsync(AdvertisementParams advertisementParams, CancellationToken cancellationToken);
 }
--- client/src/app/components/advertisement/advertisement-list/advertisement-list.component.ts
+++ client/src/app/components/advertisement/advertisement-list/advertisement-list.component.ts
@@ -1,42 +1,45 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AdvertisementResponse } from '../../../models/advertisement-response';
-import { PaginationParams } from '../../../models/helpers/paginationParams.model';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { Pagination } from '../../../models/helpers/pagination';
 import { PaginatedResult } from '../../../models/helpers/paginatedResult';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
 import { AdvertisementCardComponent } from '../advertisement-card/advertisement-card.component';
 import { AdvertisementService } from '../../../services/advertisement.service';
+import { AdvertisementParams } from '../../../models/helpers/advertisement-params';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-advertisement-list',
   standalone: true,
   imports: [
-    AdvertisementCardComponent,
-    MatCardModule, MatIconModule, MatPaginatorModule
+    AdvertisementCardComponent, FormsModule, ReactiveFormsModule,
+    MatCardModule, MatIconModule, MatPaginatorModule, MatSelectModule, MatSliderModule
   ],
   templateUrl: './advertisement-list.component.html',
   styleUrl: './advertisement-list.component.scss'
 })
 export class AdvertisementListComponent implements OnInit {
+  private _fB = inject(FormBuilder);
   private advertisementService = inject(AdvertisementService);
-
-  advertisements: AdvertisementResponse[] = [];
-  paginationParams: PaginationParams | undefined;
+  advertisements: AdvertisementResponse[] | undefined;
+  advertisementParams: AdvertisementParams | undefined;
   pageSizeOptions = [5, 10, 25];
   pageEvent: PageEvent | undefined;
   pagination: Pagination | undefined;
 
   ngOnInit(): void {
-    this.paginationParams = new PaginationParams();
+    this.advertisementParams = new AdvertisementParams();
 
     this.getAllAdvertisements();
   }
 
   getAllAdvertisements(): void {
-    if (this.paginationParams)
-      this.advertisementService.getAllAdvertisements(this.paginationParams).subscribe({
+    if (this.advertisementParams)
+      this.advertisementService.getAllAdvertisements(this.advertisementParams).subscribe({
         next: (response: PaginatedResult<AdvertisementResponse[]>) => {
           if (response.body) {
             this.advertisements = response.body;
@@ -58,13 +61,13 @@ export class AdvertisementListComponent implements OnInit {
   }
 
   handlePageEvent(e: PageEvent): void {
-    if (this.paginationParams) {
-      if (e.pageSize !== this.paginationParams.pageSize)
+    if (this.advertisementParams) {
+      if (e.pageSize !== this.advertisementParams.pageSize)
         e.pageIndex = 0;
 
       this.pageEvent = e;
-      this.paginationParams.pageSize = e.pageSize;
-      this.paginationParams.pageNumber = e.pageIndex + 1;
+      this.advertisementParams.pageSize = e.pageSize;
+      this.advertisementParams.pageNumber = e.pageIndex + 1;
 
       this.getAllAdvertisements();
     }
